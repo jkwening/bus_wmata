f79f3417959fce0d85378e39c5e675d6607ad373
jkwening <kwening.john@gmail.com>
2021-08-03 13:39:06 -0400

    feat: init utils.py
    Create util module that has functions for accessing sections in
    config.ini and getting an aws session object.

---
+++ utils.py
@@ -0,0 +1,31 @@
+import os
+from configparser import ConfigParser
+import boto3
+
+ROOT_DIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir)
+)
+CONFIG_FILE = os.path.join(ROOT_DIR, 'config.ini')
+
+
+def config(section: str) -> dict:
+    """Returns parameters for given section of the config.in file."""
+    parser = ConfigParser()
+    parser.read(CONFIG_FILE)
+
+    # get section parameters
+    opts = dict()
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            opts[param[0]] = param[1]
+    else:
+        raise ValueError(
+            f'Section {section} not found in config.ini')
+    return opts
+
+
+def get_aws_session() -> boto3.Session:
+    """Return aws session object."""
+    aws_config = config(section='aws')
+    return boto3.Session(**aws_config)
